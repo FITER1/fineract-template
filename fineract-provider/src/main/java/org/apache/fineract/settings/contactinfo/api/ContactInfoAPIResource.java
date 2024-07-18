ee19f565152e647f417dee6ce42529fd0f3c87fe
Mwesigye John Bosco <johnboscomwesigye@gmail.com>
2024-07-18 17:48:31 +0300

    FDI-18 Pick Deepika's work

---
+++ fineract-provider/src/main/java/org/apache/fineract/settings/contactinfo/api/ContactInfoAPIResource.java
@@ -0,0 +1,140 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.fineract.settings.contactinfo.api;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.UriInfo;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import org.apache.fineract.commands.domain.CommandWrapper;
+import org.apache.fineract.commands.service.CommandWrapperBuilder;
+import org.apache.fineract.commands.service.PortfolioCommandSourceWritePlatformService;
+import org.apache.fineract.infrastructure.core.api.ApiRequestParameterHelper;
+import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
+import org.apache.fineract.infrastructure.core.serialization.ApiRequestJsonSerializationSettings;
+import org.apache.fineract.infrastructure.core.serialization.DefaultToApiJsonSerializer;
+import org.apache.fineract.infrastructure.security.service.PlatformSecurityContext;
+import org.apache.fineract.settings.contactinfo.data.ContactInfoData;
+import org.apache.fineract.settings.contactinfo.service.ContactInfoReadPlatformService;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Path("/v1/contactinfo")
+@Component
+@RequiredArgsConstructor
+public class ContactInfoAPIResource {
+
+    private static final Set<String> CONTACT_INFO_DATA_PARAMETERS = new HashSet<>(
+            Arrays.asList("id", "email", "mobileNo", "website"));
+
+    private final PortfolioCommandSourceWritePlatformService commandsSourceWritePlatformService;
+    private final ApiRequestParameterHelper apiRequestParameterHelper;
+    private final DefaultToApiJsonSerializer<ContactInfoData> toApiJsonSerializer;
+    private final ContactInfoReadPlatformService readPlatformService;
+    private final PlatformSecurityContext context;
+
+    @POST
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    public String create(@Parameter(hidden = true) final String apiRequestBodyAsJson) {
+
+        this.context.authenticatedUser().validateHasReadPermission("CONTACTINFO");
+
+        final CommandWrapper commandRequest = new CommandWrapperBuilder() //
+                .createContactInfo() //
+                .withJson(apiRequestBodyAsJson) //
+                .build(); //
+
+        final CommandProcessingResult result = commandsSourceWritePlatformService.logCommandSource(commandRequest);
+
+        return toApiJsonSerializer.serialize(result);
+    }
+
+    @GET
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    public String retrieveContactInfo(@Context final UriInfo uriInfo) {
+
+        this.context.authenticatedUser().validateHasReadPermission("CONTACTINFO");
+        final Collection<ContactInfoData> contactInfo = this.readPlatformService.retrieveContactInfo();
+        final ApiRequestJsonSerializationSettings settings = this.apiRequestParameterHelper.process(uriInfo.getQueryParameters());
+        return this.toApiJsonSerializer.serialize(settings, contactInfo, CONTACT_INFO_DATA_PARAMETERS);
+    }
+
+    @PUT
+    @Path("{contactInfoId}")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Operation(summary = "Update Contact Info", description = "Updates a Contact Info")
+    @RequestBody(required = true, content = @Content(schema = @Schema(implementation = ContactInfoData.class)))
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ContactInfoData.class))) })
+    public String updateContactInfo(@PathParam("contactInfoId") @Parameter(description = "contactInfoId") final Long contactInfoId,
+                                    @Parameter(hidden = true) final String apiRequestBodyAsJson) {
+        this.context.authenticatedUser().validateHasReadPermission("CONTACTINFO");
+
+        final CommandWrapper commandRequest = new CommandWrapperBuilder().updateContactInfo(contactInfoId)
+                .withJson(apiRequestBodyAsJson).build();
+
+        final CommandProcessingResult result = this.commandsSourceWritePlatformService.logCommandSource(commandRequest);
+
+        return this.toApiJsonSerializer.serialize(result);
+    }
+
+    @DELETE
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Operation(summary = "Delete Contact Info", description = "Deletes a Contact Info")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ContactInfoData.class))) })
+    public String deleteContactInfo() {
+        this.context.authenticatedUser().validateHasReadPermission("CONTACTINFO");
+
+        final CommandWrapper commandRequest = new CommandWrapperBuilder().deleteContactInfo().build();
+
+        final CommandProcessingResult result = this.commandsSourceWritePlatformService.logCommandSource(commandRequest);
+
+        return this.toApiJsonSerializer.serialize(result);
+    }
+
+}
---
+++ fineract-provider/src/main/java/org/apache/fineract/settings/contactinfo/service/ContactInfoWritePlatformServiceJpaRepositoryImpl.java
@@ -0,0 +1,102 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.fineract.settings.contactinfo.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.fineract.infrastructure.core.api.JsonCommand;
+import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
+import org.apache.fineract.infrastructure.core.data.CommandProcessingResultBuilder;
+import org.apache.fineract.infrastructure.core.exception.ErrorHandler;
+import org.apache.fineract.portfolio.client.domain.Client;
+import org.apache.fineract.portfolio.client.domain.ClientNonPerson;
+import org.apache.fineract.portfolio.client.exception.ClientMustBePendingToBeDeletedException;
+import org.apache.fineract.portfolio.note.domain.Note;
+import org.apache.fineract.settings.contactinfo.data.ContactInfoDataValidator;
+import org.apache.fineract.settings.contactinfo.domain.ContactInfo;
+import org.apache.fineract.settings.contactinfo.domain.ContactInfoRepository;
+import org.apache.fineract.settings.contactinfo.exception.ContactInfoNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaSystemException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+@Service
+@Slf4j
+public class ContactInfoWritePlatformServiceJpaRepositoryImpl implements ContactInfoWritePlatformService {
+
+    private final ContactInfoRepository contactInfoRepository;
+    private final ContactInfoDataValidator fromApiJsonDeserializer;
+
+    @Transactional
+    @Override
+    public CommandProcessingResult createContactInfo(final JsonCommand command) {
+
+        this.fromApiJsonDeserializer.validateForCreate(command.json());
+
+        final String mobileNo = command.stringValueOfParameterNamed(ContactInfoDataValidator.mobileNoParamName);
+        final String emailAddress = command.stringValueOfParameterNamed(ContactInfoDataValidator.emailParamName);
+        final String website = command.stringValueOfParameterNamed(ContactInfoDataValidator.websiteParamName);
+
+        final ContactInfo newContactInfo = ContactInfo.instance(website, emailAddress, mobileNo);
+
+        this.contactInfoRepository.saveAndFlush(newContactInfo);
+
+        return new CommandProcessingResultBuilder() //
+                .withCommandId(command.commandId()) //
+                .withClientId(newContactInfo.getId()) //
+                .build();
+    }
+
+    @Override
+    public CommandProcessingResult updateContactInfo(JsonCommand command) {
+
+        this.fromApiJsonDeserializer.validateForUpdate(command.json());
+
+        final Long contactInfoId = command.entityId();
+        final ContactInfo contactInfoForUpdate = this.contactInfoRepository.findById(contactInfoId)
+                .orElseThrow(() -> new ContactInfoNotFoundException(contactInfoId));
+        final Map<String, Object> changes = contactInfoForUpdate.update(command);
+
+        if (!changes.isEmpty()) {
+            this.contactInfoRepository.saveAndFlush(contactInfoForUpdate);
+        }
+
+        return new CommandProcessingResultBuilder()
+                .withCommandId(command.commandId())
+                .withEntityId(contactInfoId)
+                .with(changes)
+                .build();
+    }
+
+    @Override
+    public CommandProcessingResult deleteContactInfo() {
+
+        this.contactInfoRepository.deleteAll();
+        this.contactInfoRepository.flush();
+        return new CommandProcessingResultBuilder()
+                .build();
+    }
+
+}
