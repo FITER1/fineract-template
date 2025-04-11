524845c0898c44f06b9b4aaa0331fe00109376b6
Naphlin Akena <starnapho@gmail.com>
2025-04-11 23:52:17 +0300

    Add pentaho reports

--- fineract-core/src/main/java/org/apache/fineract/infrastructure/core/config/FineractProperties.java
+++ fineract-core/src/main/java/org/apache/fineract/infrastructure/core/config/FineractProperties.java
@@ -367,6 +367,7 @@ public class FineractProperties {
         private String bucketName;
         private String accessKey;
         private String secretKey;
+        private String region;
     }
 
     @Getter
