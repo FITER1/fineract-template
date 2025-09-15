acbcee2ad06135ae0f89da447d048e89cacfe3aa
Mwesigye John Bosco <johnboscomwesigye@gmail.com>
2025-09-15 14:06:21 +0300

    FDC-64 Fix Unable to close Recurring Deposit Account

--- fineract-report/src/main/java/org/apache/fineract/infrastructure/report/service/PentahoReportingProcessServiceImpl.java
+++ fineract-report/src/main/java/org/apache/fineract/infrastructure/report/service/PentahoReportingProcessServiceImpl.java
@@ -106,9 +106,6 @@ public class PentahoReportingProcessServiceImpl implements ReportingProcessServi
         final var reportParams = getReportParams(queryParams);
         final var locale = ApiParameterHelper.extractLocale(queryParams);
         final var language = "en";
-        logger.info("*********************************************************************************");
-        logger.info("********************************Demo  Server*************************************");
-        logger.info("************************************Reports*********************************************");
         var outputType = "HTML";
         if (StringUtils.isNotBlank(outputTypeParam)) {
             outputType = outputTypeParam;
